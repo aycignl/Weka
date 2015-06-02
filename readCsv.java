a2db09d568cf630b098881c90aa6e5950f99baf0
Gonul Ayci <aycignl@gmail.com>
2015-06-02 20:53:17 +0300

    Update readCsv.java

--- readCsv.java
+++ readCsv.java
@@ -1,4 +1,8 @@
 /*
+/*
+ * username: Gonul
+ * e-mail: gonul.ayci90@gmail.com
+
 In weka, we can use .arff file and also .csv file. 
 Weka accepts .csv file.
 This class shows how to use .csv in weka?
